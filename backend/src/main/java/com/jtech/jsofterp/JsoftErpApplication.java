027e16041e7771d946da6c4c73cf1569a7d73ca7
jewel <jewelranar420@gmail.com>
2022-09-11 13:25:06 +0600

    backend setup

---
+++ backend/src/main/java/com/jtech/jsofterp/JsoftErpApplication.java
@@ -0,0 +1,13 @@
+package com.jtech.jsofterp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsoftErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsoftErpApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jtech/jsofterp/JsoftErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jtech.jsofterp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsoftErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
